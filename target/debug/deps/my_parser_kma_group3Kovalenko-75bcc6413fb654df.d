0a52f964f5a6125bc5c6ad40222712bb3bc4bc72
Arkadii <arkadiy.delphi@gmail.com>
2023-10-18 18:44:33 +0300

    init

---
+++ target/debug/deps/my_parser_kma_group3Kovalenko-75bcc6413fb654df.d
@@ -0,0 +1,7 @@
+B:\rustParserFirstStep\my_parser_kma_group3Kovalenko\target\debug\deps\my_parser_kma_group3Kovalenko-75bcc6413fb654df.rmeta: src\lib.rs
+
+B:\rustParserFirstStep\my_parser_kma_group3Kovalenko\target\debug\deps\libmy_parser_kma_group3Kovalenko-75bcc6413fb654df.rlib: src\lib.rs
+
+B:\rustParserFirstStep\my_parser_kma_group3Kovalenko\target\debug\deps\my_parser_kma_group3Kovalenko-75bcc6413fb654df.d: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/peg_macros-307d390e3f1923df.d
@@ -0,0 +1,10 @@
+B:\rustParserFirstStep\my_parser_kma_group3Kovalenko\target\debug\deps\peg_macros-307d390e3f1923df.dll: C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\lib.rs C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\analysis.rs C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\ast.rs C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\grammar.rs C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\tokens.rs C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\translate.rs
+
+B:\rustParserFirstStep\my_parser_kma_group3Kovalenko\target\debug\deps\peg_macros-307d390e3f1923df.d: C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\lib.rs C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\analysis.rs C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\ast.rs C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\grammar.rs C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\tokens.rs C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\translate.rs
+
+C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\lib.rs:
+C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\analysis.rs:
+C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\ast.rs:
+C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\grammar.rs:
+C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\tokens.rs:
+C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\peg-macros-0.8.2\translate.rs:
