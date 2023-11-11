00d481affcbf03d256451425c4099d72f3a28f13
Arkadii <arkadiy.delphi@gmail.com>
2023-11-11 20:09:41 +0200

    final

---
+++ target/debug/deps/atty-6bac5ee3b410401a.d
@@ -0,0 +1,5 @@
+B:\rustParserFirstStep\my_parser_kma_group3Kovalenko\target\debug\deps\atty-6bac5ee3b410401a.rmeta: C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\atty-0.2.14\src\lib.rs
+
+B:\rustParserFirstStep\my_parser_kma_group3Kovalenko\target\debug\deps\atty-6bac5ee3b410401a.d: C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\atty-0.2.14\src\lib.rs
+
+C:\Users\arkad\.cargo\registry\src\index.crates.io-6f17d22bba15001f\atty-0.2.14\src\lib.rs:
